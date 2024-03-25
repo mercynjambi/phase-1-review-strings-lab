cbb678e92fb416bae92ef0a9422cbdd1d635354b
mercynjambi <njambim695@gmail.com>
2024-03-25 20:48:23 +0300

    reviewed the strings

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let  excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ${currentUser}!';
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+//  excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+
+
+// const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
